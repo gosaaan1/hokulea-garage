63b405a8bf398f874fd574cd3edda90a1634bdf2
ごうさーーーん <123862848+gosaaan1@users.noreply.github.com>
2023-06-17 05:58:33 +0900

    Changed return message

--- aws_sam_project/micro-app/src/hello_world.py
+++ aws_sam_project/micro-app/src/hello_world.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 class HelloWorld(Resource):
 
     def get(self):
-        return {'msg': 'get method'}
+        return {'msg': 'get method canary'}
 
     def post(self):
         return {'msg': 'post method'}
